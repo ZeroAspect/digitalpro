a2886926cc9b7ae8f55ca56aaf4ec85278283e10
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-10-25 08:34:36 -0300

    feat: new commit

---
+++ routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = require("./config/config.js")
+const hbs = require("express-handlebars")
+const { marked } = require("marked")
+const path = require("path")
+
+app.engine("handlebars", hbs.engine())
+app.set("view engine", "handlebars")
+app.set("views", path.join(__dirname + "/views"))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname + "/public/")))
+
+app.get('/', async(req, res)=>{
+  res.render('home')
+})
