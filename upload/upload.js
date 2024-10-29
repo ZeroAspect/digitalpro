8089a879be37a4f7ad416edcb1487bd58fe4d11e
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-10-28 22:16:47 -0300

    upload: creating upload system

---
+++ upload/upload.js
@@ -0,0 +1,19 @@
+const multer = require("multer")
+
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(nul, 'images/posts/')
+  },
+  filename: function(req, file, cb){
+    const fileExtension = file.originalname.split('.')[1]
+
+    const newFileName = require("crypto").randomBytes(32).toString('hex')
+    cb(null, `${newFileName}.${fileExtension}`)
+  }
+})
+
+
+const upload = multer({ storage })
+
+module.exports = upload
