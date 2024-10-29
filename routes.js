ce2fbe58fef121010909dafd5d5793b18f324ec5
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-10-29 10:07:48 -0300

    feat: creating foto de perfil

--- routes.js
+++ routes.js
@@ -22,6 +22,7 @@ app.set("views", path.join(__dirname + "/views"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname + "/public/")))
+app.use(express.static(path.join(__dirname + '/images')))
 
 app.get('/', async(req, res)=>{
   res.render('home')
